fb9be5426190f45726e65d01e13c5138ea17027c
Svetlana <nooch@yandex.ru>
2023-02-14 08:34:42 +0300

    bot

--- tg_game.py
+++ tg_game.py
@@ -1,26 +1,68 @@
 import telebot
 from telebot import types
+import random
+from random import randint
 
 bot = telebot.TeleBot("6251973043:AAFoQXF6XW2WqT1MyScWillM52OyzWlBRfg")
 
-@bot.message_handler(commands = ["start"])
-def button(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    but1 = types.KeyboardButton("узнать правило игры") 
-    but2 = types.KeyboardButton("играть")
-    markup.add(but1)
-    markup.add(but2)
-    bot.send_message(message.chat.id,"Выбери ниже",reply_markup=markup)
+sweets = 80
+max_sweet = 28
+user_turn = 0
+bot_turn = 0
+flag = ""
 
-@bot.message_handler(content_types = "text")
+@bot.message_handler(commands=["start"])
+def start(message):
+    global flag
+    bot.send_message(message.chat.id,f"Приветствую вас в игре!")
+    bot.send_message(message.chat.id, f"Всего в игре {sweets} конфет")
+    flag = random.choice(["user", "bot"])
+    if flag == "user":
+        bot.send_message(message.chat.id,f"Первым ходите вы")
+        controller(message)
+    else:
+        bot.send_message(message.chat.id, f"Первым ходит бот")
+        controller(message)
+        
 def controller(message):
-    print(message.text)
-    if message.text == "узнать правило игры":
-        bot.send_message(message.chat.id,"За один ход можно забрать не более чем 28 конфет")
-    elif message.text == "играть":
-        bot.send_message(message.chat.id,"На столе лежит 221 конфета")
-        bot.send_message(message.chat.id,"делайте ход")
-        
-        
-     
+    global flag
+    if sweets>0:
+        if flag == "user":
+            #mrk = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+            #key1 = telebot.types.KeyboardButton("21")
+            #key2 = telebot.types.KeyboardButton("15")
+            #mrk.add(key1)
+            #mrk.add(key2)
+            bot.send_message(message.chat.id, f"Ваш ход введите кол-во конфет от 0 до {max_sweet}")
+            bot.register_next_step_handler(message,user_input)
+        else:
+            bot_input(message)
+    else:
+        flag = "user" if flag == "bot" else "bot"
+        bot.send_message(message.chat.id, f"Победил {flag}!")
+
+def bot_input(message):
+    global sweets,bot_turn,flag
+    if sweets <= max_sweet:
+        bot_turn = sweets
+    elif sweets % max_sweet == 0:
+        bot_turn = max_sweet - 1
+    else:
+        bot_turn = sweets % max_sweet - 1
+        if bot_turn == 0:
+            bot_turn = 1
+    sweets -= bot_turn
+    bot.send_message(message.chat.id, f"бот взял {bot_turn} конфет")
+    bot.send_message(message.chat.id, f"осталось {sweets}")
+    flag = "user" if flag == "bot" else "bot"
+    controller(message)
+
+def user_input(message):
+    global flag,user_turn,sweets
+    user_turn = int(message.text)
+    sweets -= user_turn
+    bot.send_message(message.chat.id, f"осталось {sweets}")
+    flag = "user" if flag == "bot" else "bot"
+    controller(message)
+
 bot.infinity_polling()
